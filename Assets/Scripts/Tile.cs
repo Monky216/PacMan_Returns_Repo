87f87025efee45e0af9534950b42f6271a5cce89
Monky216 <90779085+Monky216@users.noreply.github.com>
2022-03-07 16:05:10 -0500

    Revert to the things

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     public bool isPortal;
+<<<<<<< HEAD
 <<<<<<< Updated upstream
     public bool isPellet;
     public bool isSuperPellet;
     public bool didConsume;
 
+=======
+>>>>>>> parent of 67921ff (Pellets consumed, majority at least)
     public GameObject portalReceiver;
 }
 =======
